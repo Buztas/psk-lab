47b8e7c0551ee31828c4b1ae119cdc479263731e
Dziugas <dziugas.budreika@gmail.com>
2025-05-19 13:06:18 +0300

    mapper fix

--- src/main/java/org/psk/lab/menuComponent/helper/mappers/MenuMapper.java
+++ src/main/java/org/psk/lab/menuComponent/helper/mappers/MenuMapper.java
@@ -1,15 +1,65 @@
 package org.psk.lab.menuComponent.helper.mappers;
 
-import org.mapstruct.Mapper;
 import org.psk.lab.menuComponent.api.dto.ItemVariationDto;
 import org.psk.lab.menuComponent.api.dto.MenuItemDto;
 import org.psk.lab.menuComponent.domain.entities.ItemVariation;
 import org.psk.lab.menuComponent.domain.entities.MenuItem;
+import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
-public interface MenuMapper {
-    MenuItemDto toDto(MenuItem entity);
-    MenuItem toEntity(MenuItemDto dto);
-    ItemVariationDto variationToDto(ItemVariation entity);
-    ItemVariation variationToEntity(ItemVariationDto dto);
+import java.util.stream.Collectors;
+
+@Component
+public class MenuMapper {
+
+    public MenuItemDto toDto(MenuItem entity) {
+        if (entity == null) return null;
+        return MenuItemDto.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .description(entity.getDescription())
+                .price(entity.getPrice())
+                .type(entity.getType())
+                .stock(entity.getStock())
+                .variations(entity.getVariations().stream()
+                        .map(this::variationToDto)
+                        .collect(Collectors.toSet()))
+                .build();
+    }
+
+    public MenuItem toEntity(MenuItemDto dto) {
+        if (dto == null) return null;
+        return MenuItem.builder()
+                .id(dto.id())
+                .name(dto.name())
+                .description(dto.description())
+                .price(dto.price())
+                .type(dto.type())
+                .stock(dto.stock())
+                .variations(dto.variations().stream()
+                        .map(this::variationToEntity)
+                        .collect(Collectors.toSet()))
+                .build();
+    }
+
+    public ItemVariationDto variationToDto(ItemVariation entity) {
+        if (entity == null) return null;
+        return ItemVariationDto.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .description(entity.getDescription())
+                .price(entity.getPrice())
+                .stock(entity.getStock())
+                .build();
+    }
+
+    public ItemVariation variationToEntity(ItemVariationDto dto) {
+        if (dto == null) return null;
+        return ItemVariation.builder()
+                .id(dto.id())
+                .name(dto.name())
+                .description(dto.description())
+                .price(dto.price())
+                .stock(dto.stock())
+                .build();
+    }
 }
